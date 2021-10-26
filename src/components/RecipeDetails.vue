ffeb11edda38b7442604d2b07c1305230dbaaaee
LadyVamp <ladyvamp666@gmail.com>
2021-10-26 17:58:36 +0300

    refactoring get current recipe

--- src/components/RecipeDetails.vue
+++ src/components/RecipeDetails.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="recipe">
-        <div v-if="loading" class="loading">Загрузка...</div>
-        <div v-if="error" class="error">
-            {{ error }}
-        </div>
         <div v-if="recipe" class="d-flex flex-column">
             <h2 class="primary--text">{{ recipe.title }}</h2>
             <v-img
@@ -35,54 +31,22 @@
 <script>
 import IconLabel from "@/components/IconLabel";
 import IconFeature from "@/components/IconFeature";
-import axios from "axios";
+import { mapActions } from "vuex";
 
 export default {
     name: "RecipeDetails",
-    data() {
-        return {
-            loading: false,
-            recipe: null,
-            error: null,
-        };
-    },   
+    computed: {
+        recipe() {
+            return this.$store.getters.getCurrentRecipe;
+        },
+    },
     created() {
-        // загружаем данные, когда представление создано
-        // и данные реактивно отслеживаются
         this.fetchData();
     },
-    watch: {
-        // при изменениях маршрута запрашиваем данные снова
-        $route: "fetchData",
-    },
     methods: {
+        ...mapActions(["fetchRecipes"]),
         fetchData() {
-            this.error = this.recipe = null;
-            this.loading = true;
-            // замените `getPost` используемым методом получения данных / доступа к API
-            this.getRecipe(this.$route.params.id, (err, recipe) => {
-                this.loading = false;
-                if (err) {
-                    this.error = err.toString();
-                } else {
-                    this.recipe = recipe;
-                }
-            });
-        },
-        getRecipe(id) {
-            axios
-                .get(
-                    "https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/7d1e3a140e09a179c41df5103ceff39904088fb4/recipes-10-19.json"
-                )
-                .then((response) => {
-                    this.recipe = response.data.recipes.find(
-                        (item) => item.id === id
-                    );
-                    this.loading = false;
-                })
-                .catch((error) => {
-                    console.error(error.message);
-                });
+            this.fetchRecipes(this.$route.params.id);
         },
     },
     components: {
--- src/store/recipeModule.js
+++ src/store/recipeModule.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 export default {
 	state: {
 		recipes: [],
+		currentRecipe: null
 	},
 	mutations: {
 		setRecipes(state, payload) {
 			state.recipes = payload
+		},
+		setCurrentRecipe(state, payload) {
+			state.currentRecipe = payload
 		},
 	},
 	getters: {
@@ -14,16 +18,20 @@ export default {
 			return state.recipes
 		},
 		getCurrentRecipe(state) {
-			console.log(state);
 			return state.currentRecipe
 		}
 	},
 	actions: {
-		fetchRecipes(context, id) {
+		fetchRecipes(context, recipeId) {
 			axios
 				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/7d1e3a140e09a179c41df5103ceff39904088fb4/recipes-10-19.json')
 				.then(response => {
 					context.commit('setRecipes', response.data.recipes);
+					if (recipeId) {
+						context.commit('setCurrentRecipe', response.data.recipes.find(
+							item => item.id === recipeId
+						));
+					}
 				}).catch(error => {
 					console.error(error.message);
 				});
