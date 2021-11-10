f4d108b811bf71e1153f5092d15e5140ba447ed7
LadyVamp <ladyvamp666@gmail.com>
2021-11-10 11:32:41 +0300

    dropdown list for search

--- src/components/RecipesList.vue
+++ src/components/RecipesList.vue
@@ -1,58 +1,81 @@
 <template>
-  <v-container fluid>
-    <h2 class="secondary--text">Список рецептов</h2>
-    <v-text-field
-      v-model="search"
-      append-icon="mdi-magnify"
-      label="Поиск по названию"
-      single-line
-    ></v-text-field>
-    <v-chip-group mandatory active-class="primary--text">
-      <v-chip
-        v-for="(label, key) in mainIngredients"
-        :key="key"
-        @click="mainIngredient = key"
-        outlined
-      >
-        <IconStaple :staple="key" />{{ label }}
-      </v-chip>
-    </v-chip-group>
-    <v-row>
-      <v-col
-        v-for="item in filteredList"
-        :key="item.id"
-        cols="12"
-        sm="6"
-        md="4"
-        xl="3"
-      >
-        <v-card width="360px" height="360px">
-          <router-link
-            :to="{
-              name: 'RecipeDetails',
-              params: { id: item.id },
-            }"
-          >
-            <v-img
-              :src="
-                item.imageLink
-                  ? item.imageLink
-                  : 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
-              "
-              class="white--text align-end"
-              height="200px"
+    <v-container fluid>
+        <h2 class="secondary--text">Список рецептов</h2>
+        <v-row>
+            <v-col class="d-flex justify-end">
+                <div class="search-select">
+                    <v-select
+                        label="Поиск"
+                        v-model="selected"
+                        :items="options"
+                        item-value="id"
+                        item-text="name"
+                        return-object
+                        @change="onChange(selected)"
+                    >
+                    </v-select>
+                </div>
+                <div>
+                    <v-text-field
+                        v-model="search"
+                        append-icon="mdi-magnify"
+                        label="Поиск"
+                        single-line
+                    ></v-text-field>
+                </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-chip-group mandatory active-class="primary--text">
+                    <v-chip
+                        v-for="(label, key) in staples"
+                        :key="key"
+                        @click="selectedStaple = key"
+                        outlined
+                    >
+                        <IconStaple :staple="key" />{{ label }}
+                    </v-chip>
+                </v-chip-group>
+            </v-col>
+        </v-row>
+
+        <v-row>
+            <v-col
+                v-for="item in filteredList"
+                :key="item.id"
+                cols="12"
+                sm="6"
+                md="4"
+                xl="3"
             >
-            </v-img>
-            <v-card-title>{{ item.title }}</v-card-title>
-          </router-link>
-          <v-card-text>
-            <IconStaple :staple="item.staple" />
-            <IconFeature :feature="item.feature" />
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
+                <v-card width="360px" height="360px">
+                    <router-link
+                        :to="{
+                            name: 'RecipeDetails',
+                            params: { id: item.id },
+                        }"
+                    >
+                        <v-img
+                            :src="
+                                item.imageLink
+                                    ? item.imageLink
+                                    : 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                            "
+                            class="white--text align-end"
+                            height="200px"
+                        >
+                        </v-img>
+                        <v-card-title>{{ item.title }}</v-card-title>
+                    </router-link>
+                    <v-card-text>
+                        <IconStaple :staple="item.staple" />
+                        <IconFeature :feature="item.feature" />
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -61,49 +84,77 @@ import IconFeature from "@/components/IconFeature";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
-  name: "RecipesList",
-  components: { IconStaple, IconFeature },
-  data() {
-    return {
-      search: "",
-      mainIngredient: "all",
-      mainIngredients: {
-        all: "Все",
-        vegetable: "Овощи и фрукты",
-        bird: "Птица",
-        meat: "Мясо",
-        fish: "Рыба и морепродукты",
-      },
-    };
-  },
-  computed: {
-    filteredList() {
-      return this.getAllRecipes().filter((recipe) => {
-        if (this.mainIngredient === "all") {
-          return recipe.title.toLowerCase().includes(this.search.toLowerCase());
-        } else if (
-          Object.keys(this.mainIngredients).includes(this.mainIngredient)
-        ) {
-          return recipe.staple === this.mainIngredient;
-        }
-      });
+    name: "RecipesList",
+    components: { IconStaple, IconFeature },
+    data() {
+        return {
+            search: "",
+            selectedStaple: "all",
+            staples: {
+                all: "Все",
+                vegetable: "Овощи и фрукты",
+                bird: "Птица",
+                meat: "Мясо",
+                fish: "Рыба и морепродукты",
+            },
+            options: [
+                { id: "title", name: "По названию" },
+                { id: "ingredients", name: "По ингредиентам" },
+            ],
+            selected: { id: "title", name: "По названию" },
+        };
     },
-  },
-  mounted() {
-    if (this.filteredList.length === 0) {
-      this.fetchRecipes();
-    }
-  },
-  methods: {
-    ...mapActions(["fetchRecipes"]),
-    ...mapGetters(["getAllRecipes"]),
-  },
+    computed: {
+        filteredList() {
+            if (this.search === "" && this.selectedStaple === "all") {
+                return this.getAllRecipes();
+            } else if (this.search !== "" && this.selected.id === "title") {
+                // поиск по названию
+                return this.getAllRecipes().filter((recipe) =>
+                    recipe.title
+                        .toLowerCase()
+                        .includes(this.search.toLowerCase())
+                );
+            } else if (
+                this.search !== "" &&
+                this.selected.id === "ingredients"
+            ) {
+                // поиск по ингредиентам
+                console.log("TODO", this.search, this.selected.id);
+                return this.getAllRecipes();
+            } else if (this.staples !== "all") {
+                return this.getAllRecipes().filter(
+                    (recipe) => recipe.staple === this.selectedStaple
+                );
+            } else {
+                return this.getAllRecipes();
+            }
+        },
+    },
+    mounted() {
+        if (this.filteredList.length === 0) {
+            this.fetchRecipes();
+        }
+    },
+    methods: {
+        ...mapActions(["fetchRecipes"]),
+        ...mapGetters(["getAllRecipes"]),
+
+        onChange(selected) {
+            console.log(selected.id);
+        },
+    },
 };
 </script>
 
 <style lang='scss' scoped>
 .v-card__title {
-  line-height: 1.25rem;
-  word-break: normal;
+    line-height: 1.25rem;
+    word-break: normal;
+}
+.search-select {
+    display: block;
+    margin-right: 5px;
+    width: 200px;
 }
 </style>
