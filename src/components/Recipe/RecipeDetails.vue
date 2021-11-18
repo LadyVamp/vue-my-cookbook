6a0dd80d076a100490aebcbd6bf23dfee70a4f8a
LadyVamp <ladyvamp666@gmail.com>
2021-11-18 17:46:49 +0300

    link to product in store

--- src/components/Recipe/RecipeDetails.vue
+++ src/components/Recipe/RecipeDetails.vue
@@ -29,8 +29,36 @@
             <h3 class="secondary--text">Ингредиенты</h3>
             <p>{{ declinationNumberOfServings }}</p>
             <p v-if="recipe.note" class="secondary--text">{{ recipe.note }}</p>
-            <div v-for="(value, name) in recipe.ingredients" :key="name">
-                {{ name }} – {{ value }}
+            <v-switch
+                v-model="isShowLinks"
+                label="Показать ссылки на продукты в Сбермаркете"
+            ></v-switch>
+            <div v-if="isShowLinks">
+                <v-select
+                    label="Магазин"
+                    v-model="selected"
+                    :items="options"
+                    item-value="value"
+                    item-text="text"
+                    return-object
+                >
+                </v-select>
+                <div>
+                    <div
+                        v-for="(value, name) in recipe.ingredients"
+                        :key="name"
+                    >
+                        <a :href="linkToProductInStore(name)" target="_blank">{{
+                            name
+                        }}</a>
+                        – {{ value }}
+                    </div>
+                </div>
+            </div>
+            <div v-if="!isShowLinks">
+                <div v-for="(value, name) in recipe.ingredients" :key="name">
+                    {{ name }} – {{ value }}
+                </div>
             </div>
         </div>
         <div class="my-4">
@@ -56,6 +84,15 @@ export default {
     data() {
         return {
             title: "Default Title",
+            isShowLinks: false,
+            options: [
+                { value: "auchan", text: "Ашан" },
+                { value: "lenta", text: "Лента" },
+                { value: "globus", text: "Глобус" },
+                { value: "okey", text: "Окей" },
+                { value: "vkusvill", text: "Вкусвилл" },
+            ],
+            selected: { value: "auchan", text: "Ашан" },
         };
     },
     metaInfo() {
@@ -88,6 +125,14 @@ export default {
         ...mapActions(["fetchRecipes"]),
         fetchData() {
             this.fetchRecipes(this.$route.params.id);
+        },
+        linkToProductInStore(ingredient) {
+            const url =
+                "https://sbermarket.ru/" +
+                `${this.selected.value}` +
+                "/search?keywords=" +
+                `${ingredient}`;
+            return url;
         },
     },
     watch: {
