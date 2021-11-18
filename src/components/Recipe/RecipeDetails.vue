4e5c22a23aabf6c0dfdfa1a87a2c53740e66b756
LadyVamp <ladyvamp666@gmail.com>
2021-11-18 18:08:58 +0300

    fix styles

--- src/components/Recipe/RecipeDetails.vue
+++ src/components/Recipe/RecipeDetails.vue
@@ -1,49 +1,67 @@
 <template>
-    <div v-if="recipe" class="d-flex flex-column">
+    <div v-if="recipe" class="d-flex flex-column mb-4">
         <BackButton />
         <h2 class="primary--text">{{ recipe.title }}</h2>
-        <div>
-            <v-img
-                :src="
-                    recipe.imageLink
-                        ? recipe.imageLink
-                        : 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
-                "
-                class="white--text align-end"
-                height="300px"
-            >
-            </v-img>
-        </div>
-        <div>
-            <IconStaple :staple="recipe.staple" />
-            <IconFeature :feature="recipe.feature" />
-            <v-btn
-                v-if="recipe.feature.includes('combo')"
-                :to="{ name: 'RecipeDetails', params: { id: recipe.comboId } }"
-                color="primary"
-            >
-                Открыть комбо-рецепт
-            </v-btn>
-        </div>
-        <div class="my-4">
-            <h3 class="secondary--text">Ингредиенты</h3>
-            <p>{{ declinationNumberOfServings }}</p>
-            <p v-if="recipe.note" class="secondary--text">{{ recipe.note }}</p>
-            <v-switch
-                v-model="isShowLinks"
-                label="Показать ссылки на продукты в Сбермаркете"
-            ></v-switch>
-            <div v-if="isShowLinks">
-                <v-select
-                    label="Магазин"
-                    v-model="selected"
-                    :items="options"
-                    item-value="value"
-                    item-text="text"
-                    return-object
+        <v-row>
+            <v-col cols="12" sm="6">
+                <v-img
+                    :src="
+                        recipe.imageLink
+                            ? recipe.imageLink
+                            : 'https://cdn.vuetifyjs.com/images/cards/cooking.png'
+                    "
+                    class="white--text align-end"
+                    height="300px"
                 >
-                </v-select>
-                <div>
+                </v-img>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" sm="6">
+                <IconStaple :staple="recipe.staple" />
+                <IconFeature :feature="recipe.feature" />
+                <v-btn
+                    v-if="recipe.feature.includes('combo')"
+                    :to="{
+                        name: 'RecipeDetails',
+                        params: { id: recipe.comboId },
+                    }"
+                    color="primary"
+                >
+                    Открыть комбо-рецепт
+                </v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" sm="6" class="my-4">
+                <h3 class="secondary--text">Ингредиенты</h3>
+                <p>{{ declinationNumberOfServings }}</p>
+                <p v-if="recipe.note" class="secondary--text">
+                    {{ recipe.note }}
+                </p>
+                <v-row>
+                    <v-col cols="8">
+                        <v-switch
+                            v-model="isShowLinks"
+                            label="Показать ссылки на продукты в Сбермаркете"
+                        ></v-switch>
+                    </v-col>
+                    <v-col>
+                        <div v-if="isShowLinks">
+                            <v-select
+                                v-if="isShowLinks"
+                                label="Магазин"
+                                v-model="selected"
+                                :items="options"
+                                item-value="value"
+                                item-text="text"
+                                return-object
+                            >
+                            </v-select>
+                        </div>
+                    </v-col>
+                </v-row>
+                <div v-if="isShowLinks">
                     <div
                         v-for="(value, name) in recipe.ingredients"
                         :key="name"
@@ -54,21 +72,26 @@
                         – {{ value }}
                     </div>
                 </div>
-            </div>
-            <div v-if="!isShowLinks">
-                <div v-for="(value, name) in recipe.ingredients" :key="name">
-                    {{ name }} – {{ value }}
+                <div v-if="!isShowLinks">
+                    <div
+                        v-for="(value, name) in recipe.ingredients"
+                        :key="name"
+                    >
+                        {{ name }} – {{ value }}
+                    </div>
                 </div>
-            </div>
-        </div>
-        <div class="my-4">
-            <h3 class="secondary--text">Шаги</h3>
-            <ol>
-                <li v-for="item in recipe.steps" :key="item.key">
-                    {{ item }}
-                </li>
-            </ol>
-        </div>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="12" sm="6" class="my-4">
+                <h3 class="secondary--text">Шаги</h3>
+                <ol>
+                    <li v-for="item in recipe.steps" :key="item.key">
+                        {{ item }}
+                    </li>
+                </ol>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
