73c988e38801e35407f91664806940edc898d9fd
LadyVamp <ladyvamp666@gmail.com>
2021-12-06 20:39:40 +0300

    fix recipes

--- src/store/recipeModule.js
+++ src/store/recipeModule.js
@@ -39,7 +39,7 @@ export default {
 		fetchRecipes(context, recipeId) {
 			context.commit('setLoading', true);
 			axios
-				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/76b754358d57b26f3a5f8e87f6a336ae0d1a33f2/recipes-12-4.json')
+				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/27454d128ac8eec0d6028ceb3491b92db6bd3957/recipes-12-4.json')
 				.then(response => {
 					// console.log('fetchRecipes', response.data.recipes);
 					const recipes = response.data.recipes.reverse().filter(item => item.title !== 'template_title');
