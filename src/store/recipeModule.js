0a0d09e6898f8ad23662bf8bae183abeb8cca383
LadyVamp <ladyvamp666@gmail.com>
2021-12-19 12:21:31 +0300

    add recipes

--- src/store/recipeModule.js
+++ src/store/recipeModule.js
@@ -39,7 +39,7 @@ export default {
 		fetchRecipes(context, recipeId) {
 			context.commit('setLoading', true);
 			axios
-				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/21234fa5c0579836a2ad8968d29bcae10a61ca8f/recipes-12-8.json')
+				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/41c24420e861e9beaff3f0dd4e08c81cc03ace5e/recipes-12-19.json')
 				.then(response => {
 					// console.log('fetchRecipes', response.data.recipes);
 					const recipes = response.data.recipes.reverse().filter(item => item.title !== 'template_title');
