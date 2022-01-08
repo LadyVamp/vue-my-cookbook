dc51e326e239271d92343a04ea0e1358b9b556a8
LadyVamp <ladyvamp666@gmail.com>
2022-01-08 13:31:49 +0300

    add recipes

--- src/store/recipeModule.js
+++ src/store/recipeModule.js
@@ -1,59 +1,65 @@
 import axios from 'axios';
 
 export default {
-	state: {
-		recipes: [],
-		currentRecipe: null,
-		recipesCount: 0,
-		isLoading: false
-	},
-	mutations: {
-		setRecipes(state, payload) {
-			state.recipes = payload
-		},
-		setCurrentRecipe(state, payload) {
-			state.currentRecipe = payload
-		},
-		setRecipesCount(state, payload) {
-			state.recipesCount = payload
-		},
-		setLoading(state, payload) {
-			state.isLoading = payload
-		}
-	},
-	getters: {
-		getAllRecipes(state) {
-			return state.recipes
-		},
-		getCurrentRecipe(state) {
-			return state.currentRecipe
-		},
-		getRecipesCount(state) {
-			return state.recipesCount
-		},
-		getLoading(state) {
-			return state.isLoading
-		}
-	},
-	actions: {
-		fetchRecipes(context, recipeId) {
-			context.commit('setLoading', true);
-			axios
-				.get('https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/f1be4da9a95cb1c69dc985e47e778f268974d173/recipes-12-31.json')
-				.then(response => {
-					// console.log('fetchRecipes', response.data.recipes);
-					const recipes = response.data.recipes.reverse().filter(item => item.title !== 'template_title');
-					context.commit('setRecipes', recipes);
-					context.commit('setRecipesCount', recipes.length);
-					context.commit('setLoading', false);
-					if (recipeId) {
-						context.commit('setCurrentRecipe', response.data.recipes.find(
-							item => item.id === recipeId
-						));
-					}
-				}).catch(error => {
-					console.error(error.message);
-				});
-		}
-	}
-}
+    state: {
+        recipes: [],
+        currentRecipe: null,
+        recipesCount: 0,
+        isLoading: false,
+    },
+    mutations: {
+        setRecipes(state, payload) {
+            state.recipes = payload;
+        },
+        setCurrentRecipe(state, payload) {
+            state.currentRecipe = payload;
+        },
+        setRecipesCount(state, payload) {
+            state.recipesCount = payload;
+        },
+        setLoading(state, payload) {
+            state.isLoading = payload;
+        },
+    },
+    getters: {
+        getAllRecipes(state) {
+            return state.recipes;
+        },
+        getCurrentRecipe(state) {
+            return state.currentRecipe;
+        },
+        getRecipesCount(state) {
+            return state.recipesCount;
+        },
+        getLoading(state) {
+            return state.isLoading;
+        },
+    },
+    actions: {
+        fetchRecipes(context, recipeId) {
+            context.commit('setLoading', true);
+            axios
+                .get(
+                    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/25f9c3cec6c6c5ede3ea0456b84ab794bfd299cd/recipes-01-08.json',
+                )
+                .then((response) => {
+                    // console.log('fetchRecipes', response.data.recipes);
+                    const recipes = response.data.recipes
+                        .reverse()
+                        .filter((item) => item.title !== 'template_title');
+                    context.commit('setRecipes', recipes);
+                    context.commit('setRecipesCount', recipes.length);
+                    context.commit('setLoading', false);
+                    if (recipeId) {
+                        context.commit(
+                            'setCurrentRecipe',
+                            response.data.recipes.find((item) => item.id === recipeId),
+                        );
+                    }
+                })
+                .catch((error) => {
+                    console.error(error.message);
+                });
+        },
+    },
+};
