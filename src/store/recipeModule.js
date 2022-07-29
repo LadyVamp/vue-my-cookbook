e7511db6cf07ebefb769bdb5ec74da4bfb0932fa
LadyVamp <ladyvamp666@gmail.com>
2022-07-29 08:37:59 +0300

    fix recipe

--- src/store/recipeModule.js
+++ src/store/recipeModule.js
@@ -40,7 +40,7 @@ export default {
             context.commit('setLoading', true);
             axios
                 .get(
-                    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/646223c3cfded6b2fe21755ce05813787654618d/recipes-2022-06-22.json',
+                    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/8eecca9f1100f2e03b1034b75d6c1dea6ca9e86c/recipes-2022-07-29.json',
                 )
                 .then((response) => {
                     // console.log('recipes', response.data.recipes);
