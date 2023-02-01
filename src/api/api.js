1a9dbe81d65629631da8186c619b27b081f56156
LadyVamp <ladyvamp666@gmail.com>
2023-02-01 20:10:20 +0300

    add recipe

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const recipes =
-    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/e32412e25dd5d585ddb72f88b36c04f8090c64d8/recipes-2023-01-02.json';
+    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/99452c67ba5c8c985fd62ac3f0f6ea8ff67844b8/recipes-2023-02-01.json';
 
 export async function getAllRecipes() {
     try {
