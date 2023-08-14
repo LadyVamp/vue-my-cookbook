94f0ea1a13cd16a6d6af9dd259f7f7433511ad90
LadyVamp <ladyvamp666@gmail.com>
2023-08-14 23:05:38 +0300

    npm start, add recipes, fix description

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const recipes =
-    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/3a8b4f2df616a42e6d229330cec0b8bb99457eb9/recipes-2023-08-07.json';
+    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/78a28ff699ba121e001963b4a2f129a755a7acf1/recipes-2023-08-14.json';
 
 export async function getAllRecipes() {
     try {
