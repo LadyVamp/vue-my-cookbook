333dbd508e55dbc952c5d8fbc01cd9c1a04a6a1d
LadyVamp <ladyvamp666@gmail.com>
2023-10-09 20:01:19 +0300

    fix recipe

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const recipes =
-    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/4e285dfd26876488401e86411479a9f42d1499bf/recipes-2023-08-14.json';
+    'https://gist.githubusercontent.com/LadyVamp/628c9e7aa0d9d26971bf9d512cef6bbe/raw/9131d407b67a243103cb2388d6ad21f20c982488/recipes-2023-10-09.json';
 
 export async function getAllRecipes() {
     try {
