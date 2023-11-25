79c422d3856c412f453a613fb8dfaeaca3d03e81
LadyVamp <ladyvamp666@gmail.com>
2023-11-25 17:59:35 +0300

    fix footer

--- src/components/Layout/TheFooter.vue
+++ src/components/Layout/TheFooter.vue
@@ -2,19 +2,12 @@
     <v-footer padless class="mt-4">
         <v-col class="text-center text-body-2 py-1" cols="12">
             © 2021—{{ new Date().getFullYear() }}
-            <a
-                href="https://ladyvamp.github.io/ng-portfolio/"
-                target="_blank"
-                title="Перейти к портфолио"
-                >Nadezhda Dyakova aka LadyVamp</a
-            >
+            <a href="https://ladyvamp.github.io/ng-portfolio/" target="_blank" title="Перейти к портфолио">
+                Nadezhda Kuzova aka LadyVamp
+            </a>
         </v-col>
         <v-col class="text-right pt-0 pb-1 caption" cols="12">
-            <a
-                href="https://ladyvamp.github.io/vue-my-cookbook/"
-                class="grey--text text--darken-3"
-                target="_blank"
-            >
+            <a href="https://ladyvamp.github.io/vue-my-cookbook/" class="grey--text text--darken-3" target="_blank">
                 {{ version }}
             </a>
         </v-col>
