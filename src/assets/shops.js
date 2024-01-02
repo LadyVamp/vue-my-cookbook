18f2b303a1da4e42c03582676facf8c3f93c363a
LadyVamp <ladyvamp666@gmail.com>
2024-01-02 13:48:24 +0300

    upd shop list

--- src/assets/shops.js
+++ src/assets/shops.js
@@ -1,5 +1,6 @@
 const shopList = [
     { value: 'auchan', text: 'Ашан' },
+    { value: 'lenta', text: 'Лента' },
     { value: 'globus', text: 'Глобус' },
     { value: 'metro', text: 'Метро' },
     { value: 'selgros', text: 'Selgros' },
