bc10c7ed6f6d4008466c8300fa48861b02f7f0a8
LadyVamp <ladyvamp666@gmail.com>
2024-08-05 10:06:45 +0300

    add shop

--- src/assets/shops.js
+++ src/assets/shops.js
@@ -1,9 +1,10 @@
 const shopList = [
     { value: 'auchan', text: 'Ашан' },
-    { value: 'lenta', text: 'Лента' },
     { value: 'globusgiper', text: 'Глобус' },
-    { value: 'okey', text: 'Окей' },
     { value: 'metro', text: 'Метро' },
+    { value: 'perekrestok', text: 'Перекресток' },
     { value: 'perekrestokvprok', text: 'Перекресток Впрок' },
+    { value: 'lenta', text: 'Лента' },
+    { value: 'okey', text: 'Окей' },
 ];
 export default shopList;
